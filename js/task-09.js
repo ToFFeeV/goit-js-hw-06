5b95afd4b7f51f290241efe0abe9cdfa008cbaad
ToFFeeV <khmiluvkiyv.ku@gmail.com>
2023-02-19 16:16:07 +0200

    Task-09 done.

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btn = document.querySelector('.change-color');
+const textEl = document.querySelector('.color');
+const body = document.body;
+
+btn.addEventListener('click', clickRandomHexColor);
+
+function clickRandomHexColor() {
+  textEl.textContent = body.style.backgroundColor = getRandomHexColor();
+}
